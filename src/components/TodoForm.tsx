8c7c29143ca4d5d3bd04e6dede269845a31ddfac
Daniel Harvilik <daniel.harvilik@hardsun.eu>
2023-12-06 13:50:48 +0100

    remove unused values

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -16,7 +16,7 @@ export const TodoForm = ({ initialTodo }: TodoFormProps) => {
     register,
     handleSubmit,
     reset,
-    formState: { errors },
+    formState: { },
   } = useForm<CreateTodoBody>({ resolver: zodResolver(todoSchema), defaultValues: initialTodo });
 
   const postTodoMutation = useMutation({
